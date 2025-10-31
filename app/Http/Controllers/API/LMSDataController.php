757b8bd6358888ca7eb58916d9c37f999dfb59de
ouedraogo7 <seven9902012@gmail.com>
2025-10-31 19:38:17 +0000

    fix: Ajouter import manquant Illuminate\Support\Facades\Http

--- app/Http/Controllers/API/LMSDataController.php
+++ app/Http/Controllers/API/LMSDataController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Http;
 use Carbon\Carbon;
 
 class LMSDataController extends Controller
